c27482601836cb1b568b24c73b1e0a4df673828b
MItchPisa76 <89701539+MItchPisa76@users.noreply.github.com>
2024-08-22 09:19:52 +0200

    Update stop.js

--- stop.js
+++ stop.js
@@ -1,6 +1,8 @@
 const io = require('socket.io-client');
 const socketClient = io.connect("http://"+process.env.CODESPACE_NAME); // Specify port if your express server is not using default port 80
 
+console.log("http://"+process.env.CODESPACE_NAME);
+
 socketClient.on('connect', () => {
   socketClient.emit('npmStop');
   setTimeout(() => {
