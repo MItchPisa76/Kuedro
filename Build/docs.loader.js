9b51e343fc7de81da3a0b0eeff5c9d35e145e242
MItchPisa76 <89701539+MItchPisa76@users.noreply.github.com>
2025-03-15 17:54:57 +0100

    up

--- Build/docs.loader.js
+++ Build/docs.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 0,
     },
-    wasmFileSize: 72179297,
+    wasmFileSize: 72179554,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
