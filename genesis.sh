74dec6c4fee6527d71c48c70f82bacb52f69da6a
MItchPisa76 <89701539+MItchPisa76@users.noreply.github.com>
2026-03-08 12:59:35 +0100

    Update genesis.sh

--- genesis.sh
+++ genesis.sh
@@ -1,6 +1,26 @@
 
 apt update
 apt install git
-git clone https://github.com/MItchPisa76/Kuedro.git
+git clone 
+
+REPO_DIR="kuedro"
+REPO_URL="https://github.com/MItchPisa76/Kuedro.git"
+  cd "$REPO_DIR" || exit
+if [ ! -d "$REPO_DIR" ]; then
+    echo "Clonazione iniziale..."
+    git clone "$REPO_URL" .
+else
+    echo "Aggiornamento con salvataggio modifiche locali..."
+      
+    # 1. Mette da parte eventuali modifiche locali fatte per sbaglio
+    git stash
+    
+    # 2. Scarica gli aggiornamenti dal server
+    git pull
+    
+    # 3. (Opzionale) Riapplica le tue modifiche sopra il nuovo codice
+    # git stash pop 
+fi
+
 
 nginx -g 'daemon off;'
